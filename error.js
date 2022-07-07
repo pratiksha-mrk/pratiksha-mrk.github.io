f019a0ef75e9b309aec1f5c442c46099ab5adf92
pratiksha-mrk <108868586+pratiksha-mrk@users.noreply.github.com>
2022-07-07 22:10:41 +0530

    error file

---
+++ error.js
@@ -0,0 +1,11 @@
+function temp(){
+	var c = document.getElementById('name').value;
+	var d = document.getElementById('length').value;
+	
+    if (c.length == 0)
+     {
+        alert("entre value of c");
+	    
+	 }
+	 
+}  
